5383883204725ec5ec8cbdfa8007fe4b1443f87c
Simon Tucker <simon@brotherlogic@gmail>
2018-11-30 19:04:11 -0800

    Fixes spec

--- recordget.go
+++ recordget.go
@@ -184,7 +184,7 @@ func (s *Server) getReleaseFromPile(ctx context.Context, t time.Time) (*pbrc.Rec
 	ctx = s.LogTrace(ctx, "PostStage", time.Now(), pbt.Milestone_MARKER)
 
 	// If the time is between 1800 and 1900 - only reveal PRE_FRESHMAN records
-	if t.Hour() >= 18 && t.Hour() <= 19 {
+	if t.Hour() >= 18 && t.Hour() < 19 {
 		pDate := int64(0)
 		for _, rc := range r.GetRecords() {
 			if rc.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_PRE_FRESHMAN {
