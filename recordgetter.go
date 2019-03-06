7bf5f133ce2a68c91ef9716c57cbbe1d5e2c19b1
Simon Tucker <simon@brotherlogic@gmail>
2019-03-05 16:59:28 -0800

    Adjusts time for getting pre freshman

--- recordgetter.go
+++ recordgetter.go
@@ -115,8 +115,8 @@ func (s *Server) getReleaseFromPile(ctx context.Context, t time.Time) (*pbrc.Rec
 		}
 	}
 
-	// If the time is between 1800 and 1900 - only reveal PRE_FRESHMAN records
-	if t.Hour() >= 18 && t.Hour() < 19 {
+	// If the time is between 1900 and 2000 - only reveal PRE_FRESHMAN records
+	if t.Hour() >= 19 && t.Hour() < 20 {
 		pDate := int64(0)
 		for _, rc := range r.GetRecords() {
 			if rc.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_PRE_FRESHMAN {
