8be8d6032d762362a05f261e84af38c98a13a843
Simon Tucker <brotherlogic@gmail.com>
2022-09-03 11:02:59 -0700

    Remove DLog line. This closes #779

--- recordgetter.go
+++ recordgetter.go
@@ -326,8 +326,6 @@ func (s *Server) dateFine(rc *pbrc.Record, t time.Time, state *pbrg.State) bool 
 			// Two days between listens
 			if t.Sub(time.Unix(score.ScoreDate, 0)) < time.Hour*12 {
 				return false
-			} else {
-				s.DLog(context.Background(), fmt.Sprintf("Skipping %v - last listen was %v (%v)", score.GetInstanceId(), time.Unix(score.ScoreDate, 0), t.Sub(time.Unix(score.ScoreDate, 0))))
 			}
 		}
 	}
