c713710e9eac8dbca494fc9c1780bf33ca77c71e
Simon Tucker <brotherlogic@gmail.com>
2023-03-06 10:29:28 -0800

    Aha

--- recordgetter.go
+++ recordgetter.go
@@ -364,6 +364,7 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 	}
 
 	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_VALIDATE, state)
+	s.CtxLog(ctx, fmt.Sprintf("SKIP %v %v", rec, err))
 	if (err != nil || rec != nil) && s.validate(rec, state) {
 		s.CtxLog(ctx, "PICKED PV")
 		return rec, err
