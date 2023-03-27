ecf5ab9f4cbd9f52ce60f1cce5e8571c049789db
Simon Tucker <brotherlogic@gmail.com>
2023-03-27 10:42:44 -0700

    Only update score count for non digital releases

--- recordgetter.go
+++ recordgetter.go
@@ -281,7 +281,9 @@ func (p *prodUpdater) update(ctx context.Context, config *pb.State, id, rating i
 		config.UnlistenedCount++
 	}
 
-	config.ScoreCount[int32(rec.GetRecord().GetMetadata().GetCategory())]++
+	if rec.GetRecord().GetMetadata().GetFiledUnder() != pbrc.ReleaseMetadata_FILE_DIGITAL {
+		config.ScoreCount[int32(rec.GetRecord().GetMetadata().GetCategory())]++
+	}
 
 	_, err = client.UpdateRecord(ctx, &pbrc.UpdateRecordRequest{Update: &pbrc.Record{Release: &pbgd.Release{InstanceId: id}, Metadata: &pbrc.ReleaseMetadata{SetRating: rating}}, Reason: "RecordScore from Getter"})
 	if err != nil {
