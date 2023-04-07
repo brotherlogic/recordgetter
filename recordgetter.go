1603d50b0ce6cc1bf4b217ddb95a59a223513ecc
Simon Tucker <brotherlogic@gmail.com>
2023-04-07 14:36:43 -0700

    Simplify picking process

--- recordgetter.go
+++ recordgetter.go
@@ -382,23 +382,6 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 		return rec, err
 	}
 
-	if state.CatCount[int32(pbrc.ReleaseMetadata_PRE_IN_COLLECTION.Number())] == 0 {
-		rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_IN_COLLECTION, state, digitalOnly)
-		if (err != nil || rec != nil) && s.validate(rec, state) {
-			s.CtxLog(ctx, "PICKED FIST PIC")
-			return rec, err
-		}
-	}
-
-	// Look for pre high school records
-	if state.CatCount[int32(pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL.Number())] == 0 {
-		rec, err = s.getInFolderWithCategory(ctx, t, int32(812802), pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL, state, digitalOnly, false)
-		if (err != nil || rec != nil) && s.validate(rec, state) {
-			s.CtxLog(ctx, "PICKED FIRST PHS")
-			return rec, err
-		}
-	}
-
 	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_VALIDATE, state, digitalOnly)
 	s.CtxLog(ctx, fmt.Sprintf("SKIP %v %v", rec, err))
 	if (err != nil || rec != nil) && s.validate(rec, state) {
@@ -412,7 +395,7 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 		return rec, err
 	}
 
-	rec, err = s.getInFolderWithCategory(ctx, t, int32(812802), pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL, state, digitalOnly, false)
+	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL, state, digitalOnly)
 	if (err != nil || rec != nil) && s.validate(rec, state) {
 		s.CtxLog(ctx, "PICKED PHS")
 		return rec, err
