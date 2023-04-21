6247064c2502095c1c2baf69052bc2840ed8561c
Simon Tucker <brotherlogic@gmail.com>
2023-04-21 12:55:36 -0700

    Support working through UL records

--- recordgetter.go
+++ recordgetter.go
@@ -401,6 +401,12 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 		return rec, err
 	}
 
+	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_UNLISTENED, state, digitalOnly)
+	if (err != nil || rec != nil) && s.validate(rec) {
+		s.CtxLog(ctx, "PICKED UL LSAT")
+		return rec, err
+	}
+
 	//Update the wait time
 	waiting.With(prometheus.Labels{"wait": "want"}).Set(float64(state.GetLastWant()))
 
