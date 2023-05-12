4abb7572ea9c3b266e8c6b264892d9480ef3a268
Simon Tucker <brotherlogic@gmail.com>
2023-05-12 10:19:34 -0700

    Move PV down the list

--- recordgetter.go
+++ recordgetter.go
@@ -382,13 +382,6 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 		return rec, err
 	}
 
-	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_VALIDATE, state, digitalOnly)
-	s.CtxLog(ctx, fmt.Sprintf("SKIP %v %v", rec, err))
-	if (err != nil || rec != nil) && s.validate(rec) {
-		s.CtxLog(ctx, "PICKED PV")
-		return rec, err
-	}
-
 	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_IN_COLLECTION, state, digitalOnly)
 	if (err != nil || rec != nil) && s.validate(rec) {
 		s.CtxLog(ctx, "PICKED PIC")
@@ -404,6 +397,14 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_UNLISTENED, state, digitalOnly)
 	if (err != nil || rec != nil) && s.validate(rec) {
 		s.CtxLog(ctx, "PICKED UL LSAT")
+		return rec, err
+	}
+
+	//P-V is for funsies
+	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_VALIDATE, state, digitalOnly)
+	s.CtxLog(ctx, fmt.Sprintf("SKIP %v %v", rec, err))
+	if (err != nil || rec != nil) && s.validate(rec) {
+		s.CtxLog(ctx, "PICKED PV")
 		return rec, err
 	}
 
