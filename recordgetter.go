59fec48253e97dd32a7023d2c6f850aadc0281c1
Simon Tucker <brotherlogic@gmail.com>
2024-02-08 08:54:37 -0800

    Fix bug

--- recordgetter.go
+++ recordgetter.go
@@ -352,14 +352,14 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 
 	//Look for a record staged to sell
 	rec, err := s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_STAGED_TO_SELL, state, typ)
-	if (err != nil || rec != nil) && s.validate(rec) {
+	if (err != nil || rec != nil) && s.validate(rec, typ) {
 		s.CtxLog(ctx, "PICKED STS")
 		return rec, err
 	}
 
 	if state.ScoreCount[int32(pbrc.ReleaseMetadata_UNLISTENED.Number())] == 0 {
 		rec, err := s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_UNLISTENED, state, typ)
-		if (err != nil || rec != nil) && s.validate(rec) {
+		if (err != nil || rec != nil) && s.validate(rec, typ) {
 			s.CtxLog(ctx, "PICKED FIRST UL")
 			return rec, err
 		}
@@ -367,7 +367,7 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 
 	if state.ScoreCount[int32(pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL.Number())] == 0 {
 		rec, err := s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL, state, typ)
-		if (err != nil || rec != nil) && s.validate(rec) {
+		if (err != nil || rec != nil) && s.validate(rec, typ) {
 			s.CtxLog(ctx, "PICKED FIRST PHS")
 			return rec, err
 		}
@@ -375,14 +375,14 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 
 	if state.ScoreCount[int32(pbrc.ReleaseMetadata_PRE_IN_COLLECTION.Number())] == 0 {
 		rec, err := s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_IN_COLLECTION, state, typ)
-		if (err != nil || rec != nil) && s.validate(rec) {
+		if (err != nil || rec != nil) && s.validate(rec, typ) {
 			s.CtxLog(ctx, "PICKED FIST PIC")
 			return rec, err
 		}
 	}
 	if state.GetUnlistenedCount() == 0 || typ != pb.RequestType_CD_FOCUS {
 		rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_UNLISTENED, state, typ)
-		if (err != nil || rec != nil) && s.validate(rec) {
+		if (err != nil || rec != nil) && s.validate(rec, typ) {
 			s.CtxLog(ctx, "PICKED UL LSAT")
 			return rec, err
 		}
@@ -390,13 +390,13 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 
 	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_IN_COLLECTION, state, typ)
 	s.CtxLog(ctx, fmt.Sprintf("FOUND PIC -> %v,%v", rec, err))
-	if (err != nil || rec != nil) && s.validate(rec) {
+	if (err != nil || rec != nil) && s.validate(rec, typ) {
 		s.CtxLog(ctx, "PICKED PIC")
 		return rec, err
 	}
 
 	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL, state, typ)
-	if (err != nil || rec != nil) && s.validate(rec) {
+	if (err != nil || rec != nil) && s.validate(rec, typ) {
 		s.CtxLog(ctx, "PICKED PHS")
 		return rec, err
 	}
@@ -404,7 +404,7 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 	//P-V is for funsies
 	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_VALIDATE, state, typ)
 	s.CtxLog(ctx, fmt.Sprintf("SKIP %v %v", rec, err))
-	if (err != nil || rec != nil) && s.validate(rec) {
+	if (err != nil || rec != nil) && s.validate(rec, typ) {
 		s.CtxLog(ctx, "PICKED PV")
 		return rec, err
 	}
