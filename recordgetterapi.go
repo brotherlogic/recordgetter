5bf4e2be3887b457d00d38e27f89cc604da02ab2
Simon Tucker <brotherlogic@gmail.com>
2025-03-19 21:31:20 -0700

    Reduce threshold to two months

--- recordgetterapi.go
+++ recordgetterapi.go
@@ -263,7 +263,7 @@ func (s *Server) Listened(ctx context.Context, in *pbrc.Record) (*pb.Empty, erro
 
 		if record.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL &&
 			record.GetMetadata().GetFiledUnder() == pbrc.ReleaseMetadata_FILE_12_INCH {
-			if time.Since(time.Unix(record.GetMetadata().DateArrived, 0)) < time.Hour*24*30*3 {
+			if time.Since(time.Unix(record.GetMetadata().DateArrived, 0)) < time.Hour*24*30*2 {
 				s.CtxLog(ctx, fmt.Sprintf("Updating %v", time.Since(time.Unix(record.Metadata.GetDateArrived(), 0))))
 				state.TwelvePhs++
 			}
