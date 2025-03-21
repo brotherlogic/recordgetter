ab2d56727dc0dfb3bee02ab57c28336d1acdde36
Simon Tucker <brotherlogic@gmail.com>
2025-03-21 07:42:12 -0700

    Also

--- recordgetter.go
+++ recordgetter.go
@@ -424,7 +424,7 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 	}
 
 	s.CtxLog(ctx, fmt.Sprintf("FOUND PHS SCORE: %v", state.TwelvePhs))
-	if typ == pb.RequestType_DEFAULT && state.TwelvePhs <= 0 {
+	if typ == pb.RequestType_DEFAULT && state.TwelvePhs <= 2 {
 		rec, err := s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL, state, typ, true)
 		if (err != nil || rec != nil) && s.validate(rec, typ) {
 			s.CtxLog(ctx, "PICKED FIRST PHS 12")
