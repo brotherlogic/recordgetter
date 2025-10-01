6ff1c184b2be0c3f10a45c4dfc3789807325c272
Simon Tucker <brotherlogic@gmail.com>
2025-10-01 16:32:09 -0700

    Do 12 inches first

--- recordgetter.go
+++ recordgetter.go
@@ -501,6 +501,14 @@ func (s *Server) getReleaseFromPile(ctx context.Context, state *pbrg.State, t ti
 		return rec, err
 	}
 
+	//Do 12 inch PHS first
+	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL, state, typ, true)
+	s.CtxLog(ctx, fmt.Sprintf("SKIP %v %v", rec, err))
+	if (err != nil || rec != nil) && s.validate(rec, typ) {
+		s.CtxLog(ctx, "PICKED Final 12 inch true PHS")
+		return rec, err
+	}
+
 	//Do PHS id we have nothing else
 	rec, err = s.getCategoryRecord(ctx, t, pbrc.ReleaseMetadata_PRE_HIGH_SCHOOL, state, typ, false)
 	s.CtxLog(ctx, fmt.Sprintf("SKIP %v %v", rec, err))
